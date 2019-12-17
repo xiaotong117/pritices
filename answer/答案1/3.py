929d69851aba20c5bec6d784df3e7c4954e74bef
443091047@qq.com <3281234zhangTT>
2019-12-17 15:50:37 +0800

    Initial commit

---
+++ answer/答案1/3.py
@@ -0,0 +1,20 @@
+"""
+输出2~99之间的素数
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-03-02
+"""
+
+import math
+
+for num in range(2, 100):
+    is_prime = True
+    #这里的范围用num开根号向上取整数，可以缩短循环范围
+    for factor in range(2, int(math.sqrt(num)) + 1):
+        if num % factor == 0:
+            is_prime = False
+            #判断为非素数后立即跳出，避免不必要的资源浪费
+            break
+    if is_prime:
+        print(num, end=' ')
